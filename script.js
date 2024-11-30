0f67554614c516f7e49481d6653b0416d7b2da20
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-11-30 16:22:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let btn=document.querySelector('input');
+
+btn.addEventListener('click',()=>{
+	let drp=document.getElementById('colorSelect');
+	let color=drp.value;
+
+let childarr=drp.children;
+
+for(let i=0;i<childarr.length;i++)
+	{
+		let ele=childarr[i];
+		if(childarr[i].value===color)
+		{
+			childarr[i].remove();
+			break;
+		}
+	}
+})
